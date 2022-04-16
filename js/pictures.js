169fba8a065bc8a2afea6dc127bc2ce42b262a07
jossybijo <90437919+jossybijo@users.noreply.github.com>
2022-04-16 11:05:09 -0400

    New Files

---
+++ js/pictures.js
@@ -0,0 +1,9 @@
+window.pictures = [
+{
+    id: "p21",
+    imageUrl: "../images/pic1.png",
+    title: "Sunset",
+    description: "A sunset is a remainder that all problems has a beginnin as well as an end.",
+    discontinued: false,
+  }
+];
